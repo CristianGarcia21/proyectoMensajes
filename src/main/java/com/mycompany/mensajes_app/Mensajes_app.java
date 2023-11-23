46f01aede75c2f63bfa4c9a1151eb984294c0da8
Cristian Garcia <cristiangarcianastar21@gmail.com>
2023-11-23 06:54:06 -0500

    Creacion del repositorio y conexion a bases de datos

---
+++ src/main/java/com/mycompany/mensajes_app/Mensajes_app.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mensajes_app;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Mensajes_app {
+
+    public static void main(String[] args) {
+        Conexion conexion = new Conexion();
+        
+        try(Connection cnx = conexion.get_connection()){
+        
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
